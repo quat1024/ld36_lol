ebd53caeb6ea5296e18e9f9705b734d3ff90f380
quat <scratch933@gmail.com>
2016-08-26 21:07:29 -0400

    ITS HAPPENING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ desktop/src/quat/ld36/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package quat.ld36.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import quat.ld36.LudumDare36VideoGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LudumDare36VideoGame(), config);
+	}
+}
---
+++ html/src/quat/ld36/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package quat.ld36.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import quat.ld36.LudumDare36VideoGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new LudumDare36VideoGame();
+        }
+}
