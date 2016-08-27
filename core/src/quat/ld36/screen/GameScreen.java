960df23f645bac05c471c77931fd6f4922bf5b33
quat <scratch933@gmail.com>
2016-08-27 13:02:35 -0400

    Uhh

--- core/src/quat/ld36/screen/GameScreen.java
+++ core/src/quat/ld36/screen/GameScreen.java
@@ -4,7 +4,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import quat.ld36.LudumDare36VideoGame;
+import quat.ld36.player.PlayerCharacter;
+
+import static com.badlogic.gdx.math.MathUtils.clamp;
+import static quat.ld36.LudumDare36VideoGame.testMap;
 
 public class GameScreen implements Screen {
 	
@@ -12,11 +17,16 @@ public class GameScreen implements Screen {
 	
 	OrthographicCamera camera;
 	
+	
+	PlayerCharacter player;
+	
 	public GameScreen(LudumDare36VideoGame game_) {
 		game = game_;
 		
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, LudumDare36VideoGame.WIDTH, LudumDare36VideoGame.HEIGHT);
+		camera.setToOrtho(false, 32,18);
+		
+		player = new PlayerCharacter(8,7,testMap);
 	}
 	
 	@Override
@@ -24,16 +34,32 @@ public class GameScreen implements Screen {
 		
 	}
 	
-	public void render(float delta) {
+	public void render(float dt) {
 		Gdx.gl.glClearColor(0.1f,0.1f,0.1f,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
+		camera.position.x += (player.renderedPos.x - camera.position.x) * 2 * dt;
+		camera.position.y += (player.renderedPos.y - camera.position.y) * 2 * dt;
+		
+		camera.position.x = clamp(camera.position.x,16,1000);
+		camera.position.y = clamp(camera.position.y,9,1000);
+		
 		camera.update();
+		
+		player.handleInput(dt);
+		player.update(dt);
+		
+		
+		game.renderer.setView(camera);
+		game.shapes.setProjectionMatrix(camera.combined);
+		
 		game.batch.begin();
-		
-		game.font.draw(game.batch,"WOW IT REALLY IS!!!!!!!!",LudumDare36VideoGame.WIDTH/3, LudumDare36VideoGame.HEIGHT/2);
-		
+		game.renderer.render();
 		game.batch.end();
+		
+		game.shapes.begin(ShapeRenderer.ShapeType.Filled);
+		player.render(game.shapes);
+		game.shapes.end();
 	}
 	
 	@Override
